c78f57b1d3a1ef0d29a6ec0981f7d09c0336fe11
lam-j <101142205+lam-j@users.noreply.github.com>
2022-03-07 23:49:49 -0800

    Initial commit

---
+++ src/components/App.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function App() {
+  return (
+    <div className="container">
+      <h1>To Do List</h1>
+      <input placeholder="thing to do!"></input>
+      <button>+</button>
+    </div>
+  );
+}
