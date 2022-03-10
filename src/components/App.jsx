046d3b2c7fea3a59549a8f46580c898d20257740
lam-j <101142205+lam-j@users.noreply.github.com>
2022-03-10 09:13:32 -0800

    Add progress bar

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -8,12 +8,27 @@ import Nav from "./Nav";
 export default function App() {
   const [itemList, setItemList] = useState([]);
 
+  // const [progress, setProgress] = useState(0);
+
+  // function ProgressBar() {
+  //   return (
+  //     <div className="progress-container">
+  //       <div className="progress-bar" style={{ width: progress }}>
+  //         {progress + "%"}
+  //       </div>
+  //     </div>
+  //   );
+  // }
+
+  // function updateBar() {}
+
   function addItem(item) {
     if (item !== "") {
       setItemList((previousItems) => {
         return [...previousItems, item];
       });
     }
+    updateBar(itemList);
   }
 
   function deleteItem(id) {
@@ -30,9 +45,19 @@ export default function App() {
         <ColorBar />
       </div>
       <div className="list-container">
+        <div
+          className="highlight"
+          style={{ backgroundColor: document.body.style.backgroundColor }}
+        ></div>
         <h1>To-Do</h1>
         <Input onAdd={addItem} />
         <ItemList onDelete={deleteItem} itemList={itemList} />
+      </div>
+      <div className="progress-container">
+        <div className="progress-total">
+          <div className="progress-bar"></div>
+        </div>
+        0%
       </div>
       <div className="nav-container">
         <Nav />
